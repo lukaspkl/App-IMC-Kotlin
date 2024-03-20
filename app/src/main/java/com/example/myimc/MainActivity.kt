8f72eb8fa9857e9eda2ae8dac8d22748ca79c905
lukaspkl <fohrenoficial@Live.com>
2024-03-19 23:38:03 -0300

    Alterando layout app

--- app/src/main/java/com/example/myimc/MainActivity.kt
+++ app/src/main/java/com/example/myimc/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btnCalcular)
+        btnCalcular.setOnClickListener {
+
+            val intent = Intent(this,ResultadoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/myimc/ResultadoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
