93baea9e714657781203ce9893ec9ed0f76aa480
lukaspkl <fohrenoficial@Live.com>
2024-03-20 00:38:38 -0300

    Criando a logica do app

--- app/src/main/java/com/example/myimc/MainActivity.kt
+++ app/src/main/java/com/example/myimc/MainActivity.kt
@@ -4,18 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnCalcular: Button
+    private lateinit var editPeso: EditText
+    private lateinit var editAltura: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnCalcular = findViewById(R.id.btnCalcular)
+        editPeso = findViewById(R.id.editPeso)
+        editAltura = findViewById(R.id.editAltura)
+
+
         btnCalcular.setOnClickListener {
 
             val intent = Intent(this,ResultadoActivity::class.java)
+            val peso = editPeso.text.toString()
+            val altura = editAltura.text.toString()
+
+            if (peso.isNotEmpty() && altura.isNotEmpty()){
+
+                intent.putExtra("peso", peso.toDouble())
+                intent.putExtra("altura", altura.toDouble())
+            }
+
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/myimc/ResultadoActivity.kt
+++ app/src/main/java/com/example/myimc/ResultadoActivity.kt
@@ -2,10 +2,45 @@ package com.example.myimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ResultadoActivity : AppCompatActivity() {
+
+
+    private lateinit var textPeso: TextView
+    private lateinit var textAltura: TextView
+    private lateinit var textResultado: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        textPeso = findViewById(R.id.textPeso)
+        textAltura = findViewById(R.id.textAltura)
+        textResultado = findViewById(R.id.textResultado)
+
+        val bundle = intent.extras
+        if (bundle != null){
+
+            val peso = bundle.getDouble("peso")
+            val altura = bundle.getDouble("altura")
+
+            textPeso.text = "Peso informado : $peso kg"
+            textAltura.text = "Altura informada : $altura M"
+
+            val imc = peso / (altura * altura)
+
+            val resultado = if (imc < 18.5){
+                "baixo"
+            } else if (imc in 18.5 .. 24.9){
+                "Normal"
+            }else if (imc in 25.0 .. 29.9){
+                "SobrePeso"
+            }else {
+                "Obeso"
+            }
+            textResultado.text = resultado
+        }
     }
 }
